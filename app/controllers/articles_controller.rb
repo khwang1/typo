279c0a15d6b0f4c09514b8b582cb31bccaf3a19c
Karen Wang <wang.karenh@gmail.com>
2013-03-24 00:48:13 -0700

    adding comments.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -146,7 +146,9 @@ class ArticlesController < ContentController
     render :text => TextFilter.find(params[:id]).commenthelp
   end
 
+# -------------------------------------------------------------------------------- 
   private
+# -------------------------------------------------------------------------------- 
 
   def verify_config
     if  ! this_blog.configured?
