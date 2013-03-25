5e821d2bc3c29e043cde32a14cb3ec9c1e5e0f2d
Karen Wang <wang.karenh@gmail.com>
2013-03-25 00:25:12 -0700

    add comments to help understanding the code.

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,12 +1,19 @@
 class Admin::CategoriesController < Admin::BaseController
   cache_sweeper :blog_sweeper
 
+  # The index action is redirected new action
   def index; redirect_to :action => 'new' ; end
+  
+  # The edit action calls the method new_or_edit
   def edit; new_or_edit;  end
 
   def new 
     # puts "!!!inside new()"
+    
+    # instantiates an empty instance of Category, which is passed to form in the view
     @category = Category.new 
+
+    # existing categories are needed in the view for display
     @categories = Category.find(:all)       
     respond_to do |format|
       format.html
@@ -28,17 +35,25 @@ class Admin::CategoriesController < Admin::BaseController
 
   def new_or_edit
     # puts "!!!inside new_or_edit()"
-    @categories = Category.find(:all)
+    @categories = Category.find(:all) #@categories instance variable is needed in 'new' view
   
-    if (params[:id].nil?)
-      # puts "!!! at if (params[:id].nil?), it is a new category"
+    if (params[:id].nil? && request.post?)
+      # puts "!!! receive Post and params[:id]=#{params[:id]}"
+
+      # recieve a Post with :id nil, thus creating a new category
       @category = Category.new(params[:category])
       save_category
       return
     else
       @category = Category.find(params[:id])
-      @category.attributes = params[:category]
+      @category.attributes = params[:category] #empty if this is a not POST from an edit form
+
+      # puts "!!! params[:id]=#{params[:id]}"
+      # puts "!!! @category=#{@category}"      
+      # puts "!!! params[:category]=#{params[:category]}"
+      
       if request.post?
+        # this is Post from an edit form
         respond_to do |format|
           format.html { save_category }
           format.js do 
@@ -50,6 +65,7 @@ class Admin::CategoriesController < Admin::BaseController
         end
         return
       end
+            
     end
     render 'new'
   end
